44ba508829b87b371ebacfcf1db9599628d33dc4
null-a <null-a@users.noreply.github.com>
2016-11-08 09:46:26 +0000

    Add sumreduce0 tensor function.

--- src/tensor.js
+++ src/tensor.js
@@ -12,4 +12,20 @@ Tensor.prototype.logGamma = function() {
   return out;
 };
 
+// Sum over rows of a matrix.
+Tensor.prototype.sumreduce0 = function() {
+  if (this.dims.length !== 2) {
+    throw new Error('sumreduce0 is only implemented for matrices.');
+  }
+  var h = this.dims[0];
+  var w = this.dims[1];
+  var out = new Tensor([h, 1]);
+  for (var i = 0; i < h; i++) {
+    for (var j = 0; j < w; j++) {
+      out.data[i] += this.data[i * w + j];
+    }
+  }
+  return out;
+};
+
 module.exports = Tensor;
