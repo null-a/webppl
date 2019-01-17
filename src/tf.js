4d316ccbc4b33a7e161e765586a0976a9ddd5b29
null-a <null-a@users.noreply.github.com>
2019-01-17 09:06:50 +0000

    Load tfjs-node based on env. var.

--- src/tf.js
+++ src/tf.js
@@ -1,6 +1,11 @@
 'use strict';
 
+var process = require('process');
+
 var tf = require('@tensorflow/tfjs-core');
-//require('@tensorflow/tfjs-node');
+
+if (process.env.WEBPPL_TFJS_NODE === '1') {
+  require('@tensorflow/tfjs-node');
+}
 
 module.exports = tf;
